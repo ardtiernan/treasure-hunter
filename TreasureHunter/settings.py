e6e7ad87af7dd481faea87b75c2efbb62bd164ab
Angas Tiernan <angas@Angass-MacBook.local>
2018-08-08 21:54:34 +1000

    update readme.

--- TreasureHunter/settings.py
+++ TreasureHunter/settings.py
@@ -21,7 +21,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
